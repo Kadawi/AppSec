c2783c96e8d249b9fde08fc95c8a58470cc41c39
Kaitlyn Wimberley <kdw325@nyu.edu>
2019-09-17 21:22:57 -0400

    Added check_word and check_words to spell.c

--- spell.c
+++ spell.c
@@ -2,20 +2,42 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "dictionary.h"
 
 bool check_word(const char* word, hashmap_t hashtable[]) 
 { 
     //Set int bucket to the output of hash_function(word). 
-    //Set hashmap_t cursor equal to hashmap[bucket]. 
+	int bucket = hash_function(word);
+    //Set hashmap_t cursor equal to hashmap[bucket].
+	node* cursor = hashtable[bucket];	
     //While cursor is not NULL: 
+	while(cursor != NULL){
         //If word equals cursor->word: 
+		if(word == cursor->word) return 1;
+	//return True. 
+	//set curosr to cursor->next
+		cursor = cursor->next;
+	}
+    //set int bucket to the outpush of hash_function(word)
+	int lower_length = strlen(word);
+	char lower_word[lower_length];
+	for(int i = 0; i < lower_length; i++){
+		lower_word[i] = tolower(word[i]);
+	}
+	bucket = hash_function(lower_word);
+    //set hashmap_t cursor equal to hashmap [bucket]
+    	cursor = hashtable[bucket];
+    //while cursor is not NULL
+	while(cursor != NULL){
+    	//if lower_case(word) equals curosr->word: 
             //return True. 
-        //Else if lower_case(word) equals curosr->word: 
-            //return True. 
+		if(lower_word == cursor->word) return 1;
         //Set curosr to cursor->next. 
-    //return False. 
-    return 1;
+		cursor = cursor->next;
+	}
+	    	//return False. 
+    return 0;
 } 
  
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) { 
@@ -58,7 +80,7 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
 			new_node->next = hashtable[bucket];
 //Set hashtable[bucket] to new_node. 
 			hashtable[bucket] = new_node;
-		}
+		}	
 	}
 	//Close dict_file. 
  	fclose(dict_file);
@@ -68,14 +90,41 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
 int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) 
 { 
     //Set int num_misspelled to 0. 
-    //While line in fp is not EOF (end of file): 
+	int num_misspelled = 0;
+    //check if file pointer is valid
+	if (fp == NULL) {
+		printf("File pointer is null");
+		exit(1);
+	}		
+	char* line_buffer[4096];
+	char* word[LENGTH+1];
+	int index = 0;	
+    //While line in fp is not EOF (end of file):
+	while ((fgets(*line_buffer, sizeof(line_buffer), fp) != NULL)) {
         //Read the line. 
         //Split the line on spaces. 
+		for(int i = 0; i<sizeof(line_buffer); i++){
         //For each word in line: 
             //Remove punctuation from beginning and end of word. 
+	    		if((!(isalpha(*line_buffer[i])) && index == 0) || (!(isalpha(*line_buffer[i])) && (*line_buffer[i+1] == ' '))){
+			}
+			else if(*line_buffer[i] == ' '){
+				*word[index] = '\0';
+				index = 0;
+				if(!check_word(*word, hashtable)){
+					misspelled[num_misspelled] = *word;
+					num_misspelled++;
+				}
+			}
+			else {
+				*word[index] = *line_buffer[i];
+				index++;
+			}
+		}
+	}
             //If not check_word(word): 
                 //Append word to misspelled. 
                 //Increment num_misspelled. 
     //Return num_misspelled. 
-    return 1;
+    return num_misspelled;
 }
